380179adb553c7dcf3355025b68bbde3672ad2b6
jessicasena <jessicasenasouza@gmail.com>
2021-12-29 16:28:19 -0500

    updates to baseline 2

--- baselines_experiment_1.py
+++ baselines_experiment_1.py
@@ -9,7 +9,7 @@ from scipy import stats as st
 from torch.utils.data import DataLoader
 
 from utils.data import SensorDataset
-from utils.models import MetaSenseModel
+from utils.models import MetaSenseModel, MiniMetaSenseModel
 from utils.utils import train, test
 
 
@@ -135,6 +135,16 @@ def run_baseline1(args):
 
     return base1_results
 
+def freeze(model,bn_freeze=True):
+    for name,param in model.named_parameters():
+        if bn_freeze:
+            param.requires_grad = False
+        elif name.find('bn') == -1:
+            param.requires_grad = False
+
+def unfreeze(model):
+    for param in model.parameters():
+        param.requires_grad = True
 
 def run_baseline2(args):
     device = torch.device('cuda' if args.use_cuda
@@ -148,7 +158,7 @@ def run_baseline2(args):
     results = {}
 
     for source in source_datasets:
-        print(f'Training using {source} as source dataset...')
+        print(f'Training using {source} as source dataset...\n', flush=True)
         # Train using as source each of the datasets.
         train_data, train_labels = dataset['Xy_train'][0][source]
         train_data, train_labels = np.array(train_data).squeeze(), np.array(train_labels)
@@ -177,6 +187,7 @@ def run_baseline2(args):
         cum_acc, cum_f1, cum_recall = [], [], []
 
         for fold_idx, fold in enumerate(folds):
+            print(f'Finetuning to target....\n', flush=True)
             model = MetaSenseModel(len(source_labels2idx))
 
             labels2idx = {k: idx for idx, k in enumerate(np.unique(dataset['y_test']))}
@@ -186,6 +197,7 @@ def run_baseline2(args):
                 model.load_state_dict(torch.load(f, map_location=device))
 
             model.classifier[-1] = nn.Linear(64, len(labels2idx))
+            freeze(model.features)
 
             if args.use_cuda:
                 model = model.cuda()
@@ -203,9 +215,10 @@ def run_baseline2(args):
 
             test_loader = DataLoader(test_set, batch_size=args.batch_size, num_workers=args.num_workers, pin_memory=True)
 
-            ft_optim = torch.optim.Adam(model.parameters(), lr=1e-5)
+            ft_optim = torch.optim.Adam(model.parameters(), lr=5e-4)
+            #ft_optim = torch.optim.Adam(model.parameters(), lr=args.learning_rate)
 
-            train(model, train_loader, ft_optim, criterion, epochs=args.num_epochs, use_cuda=args.use_cuda)
+            train(model, train_loader, ft_optim, criterion, epochs=25, use_cuda=args.use_cuda)
 
             metrics = test(model, test_loader, use_cuda=args.use_cuda)
 
@@ -225,7 +238,7 @@ def run_baseline2(args):
         ci_recall = st.t.interval(0.9, len(cum_recall) -1, loc=np.mean(cum_recall), scale=st.sem(cum_recall))
         #confusion_matrix = sum(cum_conf_matrices)
 
-        results[f'baseline3_{source}_{args.num_shots}_shot'] = {
+        results[f'baseline2_{source}_{args.num_shots}_shot'] = {
             'accuracy': '{:.2f} ± {:.2f}'.format(np.mean(cum_acc) * 100, abs(np.mean(cum_acc) - ci_mean[0]) * 100),
             'f1-score': '{:.2f} ± {:.2f}'.format(np.mean(cum_f1) * 100, abs(np.mean(cum_f1) - ci_f1[0]) * 100),
             'recall': '{:.2f} ± {:.2f}'.format(np.mean(cum_recall) * 100, abs(np.mean(cum_recall) - ci_recall[0]) * 100)
@@ -251,6 +264,9 @@ if __name__ == '__main__':
     parser.add_argument('--num-workers', type=int, default=1, help='Number of workers to use for data-loading')
     parser.add_argument('--verbose', action='store_true')
     parser.add_argument('--use-cuda', action='store_true')
+    parser.add_argument('--base0', action='store_true')
+    parser.add_argument('--base1', action='store_true')
+    parser.add_argument('--base2', action='store_true')
 
     args = parser.parse_args()
     args.num_ways = 3
@@ -265,19 +281,27 @@ if __name__ == '__main__':
     results = {}
 
     # run baseline 0
-    for num_shots in ['no', 1, 5, 10]:
-        args.num_shots = num_shots
-        results[f'baseline0_{num_shots}_shot'] = run_baseline0(args)
+    if args.base0:
+        print('Running baseline 0\n', flush=True)
+        for num_shots in ['no', 1, 5, 10]:
+            args.num_shots = num_shots
+            results[f'baseline0_{num_shots}_shot'] = run_baseline0(args)
 
     # run baseline 1
-    # cross dataset WITHOUT transfer learning
-    results.update(run_baseline1(args))
+    if args.base1:
+        print("Running baseline 1\n", flush=True)
+        # cross dataset WITHOUT transfer learning
+        results.update(run_baseline1(args))
 
     # run baseline 2
-    # cross dataset WITH transfer learning
-    for num_shots in ['no', 1, 5, 10]:
-        args.num_shots = num_shots
-        results.update(run_baseline2(args))
+    if args.base2:
+        print("Running baseline 2\n", flush=True)
+        # cross dataset WITH transfer learning
+        for num_shots in ['no', 1, 5, 10]:
+        #for num_shots in [1]:
+            print("Shot: {}\n".format(num_shots), flush=True)
+            args.num_shots = num_shots
+            results.update(run_baseline2(args))
 
     table = {'experiment': []}
     for baseline, result in results.items():
@@ -291,3 +315,4 @@ if __name__ == '__main__':
     outpath = os.path.join(args.output, args.filepath.split(os.sep)[-1][:-4]) + '.txt'
     with open(outpath, 'w') as f:
         f.write(results_df.to_markdown())
+        print(results_df.to_markdown())
